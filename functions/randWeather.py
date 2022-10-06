0d414b161beea7d83482d16e6e76676c07654e9a
DeK0n <dv.knnv@gmail.com>
2022-10-06 15:12:12 +0300

    ns

--- functions/randWeather.py
+++ functions/randWeather.py
@@ -1,6 +1,7 @@
 #random weather function
 import random
 x=random.randint(1,100)
+# current chance of bad weather is set to 20%
 if x < 80:
     weather = "Weather is good"
     landingCondition = 1

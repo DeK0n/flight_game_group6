4204c89b7b3b6114bd41f380466377dc2b93c781
DeK0n <dv.knnv@gmail.com>
2022-11-30 14:08:24 +0200

    commit

--- main.py
+++ main.py
@@ -39,10 +39,8 @@ weatherCheck = 50  # in credits cr
 
 app = Flask(__name__)
 
-# check airport by ICAO
 
-
-@app.route('/airport/<icao>')
+@app.route('/airport/<icao>')  # check airport by ICAO
 def airport_check(icao):
     sql = "SELECT name, municipality from airport where ident ='" + icao + "'"
     cursor = connection.cursor()
@@ -55,10 +53,8 @@ def airport_check(icao):
         name+'", "Location":"'+location+'"}'
     return response
 
-# info (information about available destinations)
 
-
-@app.route('/get_info')
+@app.route('/get_info')  # info (information about available destinations)
 def getInfo():
     # currently large airports in Europe
     sql = "SELECT ident,municipality,iso_country from airport where continent ='EU' and type = 'large_airport' "
@@ -70,7 +66,6 @@ def getInfo():
 
 
 # status (printing information about current balance, position,visited countries)
-
 @app.route('/get_status')
 def getStatus():
     response = '{"co2":"'+str(playerCredits)+'", ""visitedNumber:"' + str(playerVisited)+'", "visitedList":"' + \
@@ -78,9 +73,8 @@ def getStatus():
         str(playerPosition)+'"}'  # modify format to json
     return response
 
-# position (select position from database)
 
-@app.route('/get_position/<ICAO>')
+@app.route('/get_position/<ICAO>')  # position (select position from database)
 def getPosition(icao):
     sql = "SELECT latitude_deg, longitude_deg from airport where ident ='" + icao + "'"
     # print(sql)
@@ -89,9 +83,80 @@ def getPosition(icao):
     response = cursor.fetchall()
     return response
 
+
+@app.route('/get_countrycode/<ICAO>')  # country code (select from airport database)
+def getCountryCode(icao):
+    sql = "SELECT iso_country from airport where ident ='" + icao + "'"
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    country = cursor.fetchall()
+    for y in country:
+        countrycode = (y[0])
+        return countrycode
+
+
+@app.route('/get_distance/<icao1>%<icao2>') # distance (calculating distance between points)
+def getDistance(icao1, icao2):
+    a = getPosition(icao1)[0]
+    b = getPosition(icao2)[0]
+    response = round((distance.distance(a, b).km), 0)  # distance in km rounded
+    return response
+
+
+@app.route('/random') # random function set to 80%
+def randomGenerator80():
+    import random
+    x = random.randint(0, 100)
+    if x <= 80:
+        response = "True"  # in string form, change to needed form
+    else:
+        response = "False"
+    return response
+
+
+# flask finish statement, all requests are above
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
+#  <- FLASK
+
+# FUNCTIONS FROM old version - some still in use
+
+# info (information about available destinations)
+
+
+def getInfo():
+    sql = "SELECT ident,municipality,iso_country from airport where continent ='EU' and type = 'large_airport' "
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print("List of airports:")
+    for x in result:
+        print("Country:", x[2], " ", "Id:", x[0], " ", "Municipality:", x[1])
+
+# status (printing information about current balance, position,visited countries)
+
+
+def getStatus():
+    print("Your curent status:")
+    print("Credits: "+str(playerCredits))
+    print("Visited countries: "+str(playerVisited)+" : "+str(playerVisitedSet))
+    print("Current position: "+str(playerPosition))
+
+# position (select position from database)
+
+
+def getPosition(icao):
+    sql = "SELECT latitude_deg, longitude_deg from airport where ident ='" + icao + "'"
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    return cursor.fetchall()
+
 # country code (select from airport database)
 
-@app.route('/get_countrycode/<ICAO>')
+
 def getCountryCode(icao):
     sql = "SELECT iso_country from airport where ident ='" + icao + "'"
     # print(sql)
@@ -104,32 +169,25 @@ def getCountryCode(icao):
 
 # distance (calculating distance between points)
 
-@app.route('/get_countrycode/<ICAO>')
+
 def getDistance(playerPosition, playerDestination):
     icao = playerPosition
     a = getPosition(icao)[0]
     icao = playerDestination
     b = getPosition(icao)[0]
     #print(a, b)
-    response = round((distance.distance(a, b).km), 0)  # distance in km rounded
-    return response
+    return round((distance.distance(a, b).km), 0)  # distance in km rounded
+
 
 # random weather function
 
-@app.route('/random')
-def randomGenerator80():
+
+def weather():
     import random
     x = random.randint(0, 100)
+    # current chance of bad weather is set to 20%
     if x <= 80:
-        response = "True" #in string form, change to needed form
+        landing = True
     else:
-        response = "False"
-    return response
-
-# flask finish statement, all requests are above
-if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=5000)
-
-#  <- FLASK
-
-
+        landing = False
+    return landing

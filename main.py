be5d03c8dfe8c1012972174d11df4a28b8ea6e88
DeK0n <dv.knnv@gmail.com>
2022-11-30 16:31:13 +0200

    com

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask_cors import CORS         # this is needed if you want JavaScript acce
 import math
 import json
 import random
+import requests
 
 # CONNECTION TO DATABASE - !!! use your user and password, host can be localhost or 127.0.0.0
 
@@ -34,93 +35,15 @@ playerGoal = 5  # countries to visit ! change
 weatherPenalty = 200  # in credits cr
 weatherCheck = 50  # in credits cr
 
-
-# FLASK ->
-
-app = Flask(__name__)
-
-
-@app.route('/airport/<icao>')  # check airport by ICAO
-def airport_check(icao):
-    sql = "SELECT name, municipality from airport where ident ='" + icao + "'"
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    response1 = cursor.fetchall()
-    for i in response1:
-        name = i[0]
-        location = i[1]
-    response = '{"ICAO":"'+icao+'", "Name":"' + \
-        name+'", "Location":"'+location+'"}'
-    return response
-
-
-@app.route('/get_info')  # info (information about available destinations)
-def getInfo():
-    # currently large airports in Europe
-    sql = "SELECT ident,municipality,iso_country from airport where continent ='EU' and type = 'large_airport' "
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    response = cursor.fetchall()
-    #response = sql.dumps(cursor.fetchall())
-    return response
-
-
-# status (printing information about current balance, position,visited countries)
-@app.route('/get_status')
-def getStatus():
-    response = '{"co2":"'+str(playerCredits)+'", ""visitedNumber:"' + str(playerVisited)+'", "visitedList":"' + \
-        str(playerVisitedSet)+'", "position":"' + \
-        str(playerPosition)+'"}'  # modify format to json
-    return response
-
-
-@app.route('/get_position/<ICAO>')  # position (select position from database)
-def getPosition(icao):
-    sql = "SELECT latitude_deg, longitude_deg from airport where ident ='" + icao + "'"
-    # print(sql)
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    response = cursor.fetchall()
-    return response
-
-
-@app.route('/get_countrycode/<ICAO>')  # country code (select from airport database)
-def getCountryCode(icao):
-    sql = "SELECT iso_country from airport where ident ='" + icao + "'"
-    # print(sql)
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    country = cursor.fetchall()
-    for y in country:
-        countrycode = (y[0])
-        return countrycode
-
-
-@app.route('/get_distance/<icao1>%<icao2>') # distance (calculating distance between points)
-def getDistance(icao1, icao2):
-    a = getPosition(icao1)[0]
-    b = getPosition(icao2)[0]
-    response = round((distance.distance(a, b).km), 0)  # distance in km rounded
-    return response
-
-
-@app.route('/random') # random function set to 80%
-def randomGenerator80():
-    import random
-    x = random.randint(0, 100)
-    if x <= 80:
-        response = "True"  # in string form, change to needed form
-    else:
-        response = "False"
-    return response
-
-
-# flask finish statement, all requests are above
-if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=5000)
-#  <- FLASK
-
 # FUNCTIONS FROM old version - some still in use
+
+# weather rquest from openweather API - remake to @app.route
+cityName = input("Enter city name: ")
+request = "https://api.openweathermap.org/data/2.5/weather?q=" + \
+    cityName+"&appid={APIkey}&units=metric" # paste your openweathermap api key where {APIkey} without brackets
+response1234 = requests.get(request).json()
+print("Weather conditions in "+str(cityName)+": ")
+print(str(response1234["weather"][0]["description"])+" and "+ str(response1234["main"]["temp"])+" C")
 
 # info (information about available destinations)
 
@@ -191,3 +114,91 @@ def weather():
     else:
         landing = False
     return landing
+
+# FLASK ->
+
+
+app = Flask(__name__)
+
+
+@app.route('/airport/<icao>')  # check airport by ICAO
+def airport_check(icao):
+    sql = "SELECT name, municipality from airport where ident ='" + icao + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    response1 = cursor.fetchall()
+    for i in response1:
+        name = i[0]
+        location = i[1]
+    response = '{"ICAO":"'+icao+'", "Name":"' + \
+        name+'", "Location":"'+location+'"}'
+    return response
+
+
+@app.route('/get_info')  # info (information about available destinations)
+def getInfo():
+    # currently large airports in Europe
+    sql = "SELECT ident,municipality,iso_country from airport where continent ='EU' and type = 'large_airport' "
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    response = cursor.fetchall()
+    #response = sql.dumps(cursor.fetchall())
+    return response
+
+
+# status (printing information about current balance, position,visited countries)
+@app.route('/get_status')
+def getStatus():
+    response = '{"co2":"'+str(playerCredits)+'", ""visitedNumber:"' + str(playerVisited)+'", "visitedList":"' + \
+        str(playerVisitedSet)+'", "position":"' + \
+        str(playerPosition)+'"}'  # modify format to json
+    return response
+
+
+@app.route('/get_position/<ICAO>')  # position (select position from database)
+def getPosition(icao):
+    sql = "SELECT latitude_deg, longitude_deg from airport where ident ='" + icao + "'"
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    response = cursor.fetchall()
+    return response
+
+
+# country code (select from airport database)
+@app.route('/get_countrycode/<ICAO>')
+def getCountryCode(icao):
+    sql = "SELECT iso_country from airport where ident ='" + icao + "'"
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    country = cursor.fetchall()
+    for y in country:
+        countrycode = (y[0])
+        return countrycode
+
+
+# distance (calculating distance between points)
+@app.route('/get_distance/<icao1>%<icao2>')
+def getDistance(icao1, icao2):
+    a = getPosition(icao1)[0]
+    b = getPosition(icao2)[0]
+    response = round((distance.distance(a, b).km), 0)  # distance in km rounded
+    return response
+
+
+@app.route('/random')  # random function set to 80%
+def randomGenerator80():
+    import random
+    x = random.randint(0, 100)
+    if x <= 80:
+        response = "True"  # in string form, change to needed form
+    else:
+        response = "False"
+    return response
+
+
+# flask finish statement, all requests are above
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
+#  <- FLASK

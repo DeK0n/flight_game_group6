c4faec13c532a84bf6dbc63142713a2e9534cc0f
DeK0n <dv.knnv@gmail.com>
2022-12-11 13:25:19 +0200

    final board added

--- v2/gamescripts.js
+++ v2/gamescripts.js
@@ -9,7 +9,7 @@ const markerList = [];
 let marker;
 let daysCounter = 7;
 // map---->
-var map = L.map('map', {
+var map = L.map("map", {
   center: [52, 12],
   zoom: 4,
   zoomControl: false,
@@ -19,7 +19,6 @@ var map = L.map('map', {
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 4,
   minZoom: 4,
-  
 
   // attribution:
   //   '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -29,13 +28,15 @@ function initializeMarks() {
   console.log(airportsList);
   for (let i = 0; i < airportsList.length; i++) {
     marker = L.marker([
-      +airportsList[i].latitude, +airportsList[i].longitude,
+      +airportsList[i].latitude,
+      +airportsList[i].longitude,
     ]).addTo(map);
     // marker.bindPopup(`${airportsList[i].city}`); //.openOn(map)
-    marker.on('click', (e) => {
-      destinationIcao = airportsList[i].icao
-      destinationText.innerText = airportsList[i].city
-      document.getElementById("weather-destination").innerText=airportsList[i].weather;
+    marker.on("click", (e) => {
+      destinationIcao = airportsList[i].icao;
+      destinationText.innerText = airportsList[i].city;
+      document.getElementById("weather-destination").innerText =
+        airportsList[i].weather;
     });
     markerList.push(marker);
   }
@@ -101,14 +102,19 @@ async function gameStart() {
   //first request to change name and get starting info
   playerInfo = await changeName();
   if (playerInfo) {
-    document.querySelector("#player-name").innerHTML = `${playerInfo.name}'s votes`;
+    document.querySelector(
+      "#player-name"
+    ).innerHTML = `${playerInfo.name}'s votes`;
     document.querySelector("#player-votes").innerHTML = playerInfo.votes;
     document.querySelector("#player-co2").innerHTML = playerInfo.co2;
-    document.getElementById("player-position").innerHTML = playerInfo.positionCity;
+    document.getElementById("player-position").innerHTML =
+      playerInfo.positionCity;
   }
   opponentInfo = await getOpponent();
   if (opponentInfo) {
-    document.querySelector("#opponent-name").innerHTML =`${opponentInfo.name}'s votes`;
+    document.querySelector(
+      "#opponent-name"
+    ).innerHTML = `${opponentInfo.name}'s votes`;
     document.querySelector("#opponent-votes").innerHTML = opponentInfo.votes;
   }
   airportsList = await getAirports();
@@ -116,39 +122,49 @@ async function gameStart() {
     initializeMarks();
     console.log(marker);
   }
+   document.getElementById("main-container").classList.remove("opacity-zero");
 }
 async function gameTurn() {
   //modifications on each press of main button
-  
+
   await modifyOneTurn(destinationIcao);
   if (modifyOneTurn) {
     playerInfo = await getPlayer();
     if (playerInfo) {
-      document.querySelector("#player-name").innerHTML = `${playerInfo.name}'s votes`;
+      document.querySelector(
+        "#player-name"
+      ).innerHTML = `${playerInfo.name}'s votes`;
       document.querySelector("#player-votes").innerHTML = playerInfo.votes;
       document.querySelector("#player-co2").innerHTML = playerInfo.co2;
-      document.getElementById("player-position").innerHTML = playerInfo.positionCity;
+      document.getElementById("player-position").innerHTML =
+        playerInfo.positionCity;
     }
     opponentInfo = await getOpponent();
     if (opponentInfo) {
-      document.querySelector("#opponent-name").innerHTML =`${opponentInfo.name}'s votes`;
+      document.querySelector(
+        "#opponent-name"
+      ).innerHTML = `${opponentInfo.name}'s votes`;
       document.querySelector("#opponent-votes").innerHTML = opponentInfo.votes;
     }
     airportsList = await getAirports();
   }
-  destinationText.innerText = '- - -';
-  document.getElementById("weather-destination").innerText='- - -';
-  daysCounter=daysCounter-1
+  destinationText.innerText = "- - -";
+  document.getElementById("weather-destination").innerText = "- - -";
+  daysCounter = daysCounter - 1;
   document.getElementById("days-counter").innerHTML = `${daysCounter} days`;
 
-  if (daysCounter == 1){
-    document.getElementById("game-button").innerHTML = "Confirm last flight and go to elections";
+  if (daysCounter == 1) {
+    document.getElementById("game-button").innerHTML =
+      "Confirm last flight and go to elections";
     document.getElementById("days-counter").innerHTML = `${daysCounter} day`;
+  }
+  if (daysCounter == 0) {
+    document.getElementById("final-board").classList.remove("hidden");
   }
 }
 
 // game
 gameStart();
 const destinationText = document.getElementById("player-destination");
-destinationText.innerText = '- - -';
+destinationText.innerText = "- - -";
 document.getElementById("days-counter").innerHTML = `${daysCounter} days`;
